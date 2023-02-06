3c165fe29430ab37544e427425eb9b6026aad1ba
Maruf Hossain <marufrony48@gmail.com>
2023-02-06 22:34:27 +0600

    book in one form

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -16,105 +16,92 @@ const Book = () => {
         </div>
 
         <div class="container">
-          <div class="left">
-            <form>
-              Your Name
-              <input
-                type="text"
-                name=""
-                placeholder="Enter name"
-                required
-              ></input>
-              Contact Number
-              <input
-                type="text"
-                name=""
-                placeholder="Enter Your Number"
-                required
-              ></input>
-              Email
-              <input
-                type="text"
-                name=""
-                placeholder="Enter email"
-                required
-              ></input>
-              Address
-              <input
-                type="text"
-                name=""
-                placeholder="Enter address"
-                required
-              ></input>
-              NID/Passport Number
-              <input
-                type="text"
-                name=""
-                placeholder="NID/Passport Number"
-                required
-              ></input>
-              No. Of Traveler
-              <input
-                type="text"
-                name=""
-                placeholder="No. Of Traveler"
-                required
-              ></input>
-              <div id="zip">
-                <label>
-                  Package Name
-                  <select>
-                    <option>Choose State..</option>
-                    <option>Jaflong</option>
-                    <option>Sajek</option>
-                    <option>Rangamati</option>
-                    <option>Bandarban</option>
-                  </select>
-                </label>
-                <label>
-                  No of Child (Age:6 or below)
-                  <input
-                    type="number"
-                    name=""
-                    placeholder="No of Child"
-                  ></input>
-                </label>
-              </div>
-            </form>
-          </div>
-
-          <div class="right">
+          <form>
+            Your Name
+            <input
+              type="text"
+              name=""
+              placeholder="Enter name"
+              required
+            ></input>
+            Contact Number
+            <input
+              type="text"
+              name=""
+              placeholder="Enter Your Number"
+              required
+            ></input>
+            Email
+            <input
+              type="text"
+              name=""
+              placeholder="Enter email"
+              required
+            ></input>
+            Address
+            <input
+              type="text"
+              name=""
+              placeholder="Enter address"
+              required
+            ></input>
+            NID/Passport Number
+            <input
+              type="text"
+              name=""
+              placeholder="NID/Passport Number"
+              required
+            ></input>
+            No. Of Traveler
+            <input
+              type="text"
+              name=""
+              placeholder="No. Of Traveler"
+              required
+            ></input>
+            <div id="zip">
+              <label>
+                Package Name
+                <select>
+                  <option>Choose State..</option>
+                  <option>Jaflong</option>
+                  <option>Sajek</option>
+                  <option>Rangamati</option>
+                  <option>Bandarban</option>
+                </select>
+              </label>
+              <label>
+                No of Child (Age:6 or below)
+                <input type="number" name="" placeholder="No of Child"></input>
+              </label>
+            </div>
+            <br></br>
             <h3>PAYMENT</h3>
-            <form>
-              Accepted Card <br></br>
-              <br></br>
-              Credit card number
-              <input
-                type="text"
-                name=""
-                placeholder="Enter card number"
-              ></input>
-              Exp month
-              <input type="text" name="" placeholder="Enter Month"></input>
-              <div id="zip">
-                <label>
-                  Exp year
-                  <select>
-                    <option>Choose Year..</option>
-                    <option>2022</option>
-                    <option>2023</option>
-                    <option>2024</option>
-                    <option>2025</option>
-                  </select>
-                </label>
-                <label>
-                  CVV
-                  <input type="number" name="" placeholder="CVV"></input>
-                </label>
-              </div>
-            </form>
-            <input type="submit" name="" value="Proceed to Checkout"></input>
-          </div>
+            <br></br>
+            Accepted Card <br></br>
+            <br></br>
+            Credit card number
+            <input type="text" name="" placeholder="Enter card number"></input>
+            Exp month
+            <input type="text" name="" placeholder="Enter Month"></input>
+            <div id="zip">
+              <label>
+                Exp year
+                <select>
+                  <option>Choose Year..</option>
+                  <option>2022</option>
+                  <option>2023</option>
+                  <option>2024</option>
+                  <option>2025</option>
+                </select>
+              </label>
+              <label>
+                CVV
+                <input type="number" name="" placeholder="CVV"></input>
+              </label>
+            </div>
+          </form>
+          <input type="submit" name="" value="Proceed to Checkout"></input>
         </div>
       </section>
     </div>

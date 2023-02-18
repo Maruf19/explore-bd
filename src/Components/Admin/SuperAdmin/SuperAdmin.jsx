91630edefd6bc923d6cf6353a7da70ccf23343fa
ahm-rubayed <rubayedahmed04@gmail.com>
2023-02-18 19:37:38 +0600

    adsf

--- src/Components/Admin/SuperAdmin/SuperAdmin.jsx
+++ src/Components/Admin/SuperAdmin/SuperAdmin.jsx
@@ -3,203 +3,207 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const SuperAdmin = () => {
-
-  const [user, setUser] = useState([]);
-  const { reset } = useForm();
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
-      .then((data) => setUser(data));
-  }, [user]);
+      .then((data) => setUsers(data));
+  }, [users]);
 
-   console.log (user)
+  console.log(users)
 
   return (
-    <div class="relative">
+    <div className="relative">
       <div>
         <h1 className="flex justify-center ml-[280px] text-bold font-weight-600 mt-5 mb-5 text-2xl border-bottom">
           {" "}
           Admin{" "}
         </h1>
       </div>
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400  ml-36">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400  ml-36">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Name
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Email
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Role
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Password
             </th>
 
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Action
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Maruf Hossain Rony
-            </th>
-            <td class="px-6 py-4 text-center">abc@gmail.com</td>
-            <td class="px-6 py-4 text-center">Admin</td>
-            <td class="px-6 py-4 text-center">Password</td>
+          {
+            users.map(user => user.role === "admin" &&
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td
+                  className="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 text-center">{user.email}</td>
+                <td className="px-6 py-4 text-center">{user.role}</td>
+                <td className="px-6 py-4 text-center">********</td>
 
-            <td className="px-6 py-4">
-              <div className="flex justify-end gap-4">
-                <button x-data="{ tooltip: 'Delete' }">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                    x-tooltip="tooltip"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </td>
-          </tr>
+                <td className="px-6 py-4">
+                  <div className="flex justify-end gap-4">
+                    <button x-data="{ tooltip: 'Delete' }">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                        x-tooltip="tooltip"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            )
+          }
         </tbody>
       </table>
 
       <div>
-      <h1 className="flex justify-center ml-[280px] text-bold font-weight-600 mt-5 mb-5 text-2xl border-bottom"> Editor </h1>
+        <h1 className="flex justify-center ml-[280px] text-bold font-weight-600 mt-5 mb-5 text-2xl border-bottom"> Editor </h1>
       </div>
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ml-36">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ml-36">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Name
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Email
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Role
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Password
             </th>
 
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Action
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Maruf Hossain Rony
-            </th>
-            <td class="px-6 py-4 text-center">abc@gmail.com</td>
-            <td class="px-6 py-4 text-center">Admin</td>
-            <td class="px-6 py-4 text-center">Password</td>
+        {
+            users.map(user => user.role === "editor" &&
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td
+                  className="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 text-center">{user.email}</td>
+                <td className="px-6 py-4 text-center">{user.role}</td>
+                <td className="px-6 py-4 text-center">********</td>
 
-            <td className="px-6 py-4">
-              <div className="flex justify-end gap-4">
-                <button x-data="{ tooltip: 'Delete' }">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                    x-tooltip="tooltip"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </td>
-          </tr>
+                <td className="px-6 py-4">
+                  <div className="flex justify-end gap-4">
+                    <button x-data="{ tooltip: 'Delete' }">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                        x-tooltip="tooltip"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            )
+          }
         </tbody>
       </table>
 
       <div>
-      <h1 className="flex justify-center ml-[280px] text-bold font-weight-600 mt-5 mb-5 text-2xl border-bottom">User</h1>
+        <h1 className="flex justify-center ml-[280px] text-bold font-weight-600 mt-5 mb-5 text-2xl border-bottom">User</h1>
       </div>
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ml-36">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ml-36">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Name
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Email
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Role
             </th>
-            <th scope="col" class="px-6 py-3 text-center">
+            <th scope="col" className="px-6 py-3 text-center">
               Password
             </th>
 
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Action
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Maruf Hossain Rony
-            </th>
-            <td class="px-6 py-4 text-center">abc@gmail.com</td>
-            <td class="px-6 py-4 text-center">Admin</td>
-            <td class="px-6 py-4 text-center">Password</td>
+        {
+            users.map(user => user.role === undefined &&
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td
+                  className="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 text-center">{user.email}</td>
+                <td className="px-6 py-4 text-center">user</td>
+                <td className="px-6 py-4 text-center">********</td>
 
-            <td className="px-6 py-4">
-              <div className="flex justify-end gap-4">
-                <button x-data="{ tooltip: 'Delete' }">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                    x-tooltip="tooltip"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </td>
-          </tr>
+                <td className="px-6 py-4">
+                  <div className="flex justify-end gap-4">
+                    <button x-data="{ tooltip: 'Delete' }">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                        x-tooltip="tooltip"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            )
+          }
         </tbody>
       </table>
     </div>
